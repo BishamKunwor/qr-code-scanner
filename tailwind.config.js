b4f832f7c3760c5ef922ac8c37ed47ac3e778074
Bisham Kunwor <novelian.nova@gmail.com>
2023-01-31 23:01:06 +0545

    Completed qr-code-scanner app

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Outfit", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        white: "hsl(0, 0%, 100%)",
+        lightGray: "hsl(212, 45%, 89%)",
+        grayishBlue: "hsl(220, 15%, 55%)",
+        darkBlue: "hsl(218, 44%, 22%)",
+      },
+    },
+  },
+  plugins: [],
+};
